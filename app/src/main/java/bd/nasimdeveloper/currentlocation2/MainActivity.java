e9d04bdebe5d025ce1c405af5035087bd7f5a015
sky2496 <nasimakash.just@gmail.com>
2019-04-25 13:13:14 +0600

    Add Marker on Google Map

--- app/src/main/java/bd/nasimdeveloper/currentlocation2/MainActivity.java
+++ app/src/main/java/bd/nasimdeveloper/currentlocation2/MainActivity.java
@@ -1,10 +1,16 @@
 package bd.nasimdeveloper.currentlocation2;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -12,23 +18,61 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Google api key from Google cloud My Map First Time = AIzaSyAHojaKCw4ZBZt0IurIIuMHemT3_NgW5Ho
 
 
     private FusedLocationProviderClient fusedLocationProviderClient;
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
 
+    private Double Latitude, Longitude;
+    private Geocoder geocoder;
+    private List<Address> addresses;
+
+    public MainActivity() {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        geocoder = new Geocoder(this, Locale.ENGLISH);
         locationCallback = new LocationCallback() {
 
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
+
+                if(locationResult!=null){
+                    for(Location location : locationResult.getLocations()){
+                        Latitude = location.getLatitude();
+                        Longitude = location.getLongitude();
+
+                        try {
+                           addresses = geocoder.getFromLocation(Latitude,Longitude,1);
+
+                           // addresses.get(0);
+
+                            String address = addresses.get(0).getAddressLine(0) + "\n" + addresses.get(0).getLocality() + addresses.get(0).getPostalCode();
+
+                            Toast.makeText(MainActivity.this,address,Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        // Toast.makeText(MainActivity.this,Latitude + " " + Longitude + "",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
             }
         };
 
@@ -59,5 +103,18 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+
+
+    }
+
+
+
+    public void Start_MAP(View view) {
+
+        Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+        intent.putExtra("LAT",Latitude);
+        intent.putExtra("LON",Longitude);
+
+        startActivity(intent);
     }
 }
